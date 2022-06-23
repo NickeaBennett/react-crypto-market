4e8c63fe1c5b703a45028c8f9d21a4798c11931f
NickeaBennett <nickeabennett@gmail.com>
2022-06-22 22:48:20 -0400

    Clean Slate

--- client/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as CoinCard } from './CoinCard';
-export { default as CoinCard } from './CoinCard';
-export { default as CoinCard } from './CoinCard';
-export { default as CoinCard } from './CoinCard';
-export { default as CoinCard } from './CoinCard';
