86ded0c263aecd5305820f542479bbef93e50875
NickeaBennett <nickeabennett@gmail.com>
2022-06-23 02:41:33 -0400

    Created new branch

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
-
+import { Outlet, Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div>
       <h1>React Crypto Market App</h1>
-      
+      <nav 
+        style={{
+          borderBottom: '1px solid',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to="/home">Home</Link> | {" "}
+        <Link to="/trending-coins">Trending Coins</Link> | {" "}
+        <Link to="/favorite-coins">Favorite Coins</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
